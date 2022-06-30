5573221d4b7b2b07c8794ea0d6653cecdc79e997
tae young kim <63242552+test12270@users.noreply.github.com>
2022-06-30 14:24:03 +0900

    Add files via upload

---
+++ client/src/component/Nav.js
@@ -0,0 +1,40 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import Login from "./Login";
+import PostList from "../pages/PostList";
+import "./Nav.css";
+
+function Nav() {
+  return (
+    <div className="wrapper">
+      <Link to="/">
+        <div className="logoContainer">
+          <img
+            src="img/bitcoin.png"
+            width="35"
+            height="35"
+            alt="asd"
+            // 👆 메인 페이지에 판매중인 이미지 띄우기
+          />
+
+          <div className="logoText">SangbusangJo</div>
+        </div>
+      </Link>
+
+      <div className="headerItems">
+        <Link to="/login" component={Login}>
+          <div className="headerItem">Login</div>
+        </Link>
+        <Link to="postlist" component={<PostList />}>
+          <div>
+            <a className="headerItem">Board</a>
+          </div>
+        </Link>
+
+        <div className="none"></div>
+      </div>
+    </div>
+  );
+}
+
+export default Nav;
