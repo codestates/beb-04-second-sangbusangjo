3a30dc755c404508ed2d7f548e916914b873e94d
codeMonkey-shin <sdw0711@hanmail.net>
2022-07-01 13:47:54 +0900

    ADD : Swagger 페이지 추가

--- server/routes/account.js
+++ server/routes/account.js
@@ -2,6 +2,8 @@ const router = require('express').Router();
 const {signIn,signOut,signUp,getBalance} = require('../controller/account');
 
 
+
+
 router.post('/signIn', signIn);
 router.post('/signUp', signUp);
 router.get('/signOut', signOut);
